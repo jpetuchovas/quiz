8d57494c9fc5533a5c9d36d08547fcfc8e7801cd
Justinas Petuchovas <justinas.petuchovas@gmail.com>
2018-05-20 11:11:06 +0300

    Extract routes to a separate component

---
+++ src/Routes.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import App from './components/App';
+import QuizPage from './components/quiz/QuizPage';
+import ResultContainer from './components/result/ResultContainer';
+
+const Routes = () => (
+  <Router>
+    <App>
+      <Route exact path="/" component={QuizPage} />
+      <Route path="/quiz" component={QuizPage} />
+      <Route path="/result" component={ResultContainer} />
+    </App>
+  </Router>
+);
+
+export default Routes;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import './index.css';
-import App from './components/App';
-import QuizPage from './components/quiz/QuizPage';
 import store from './store';
-import ResultContainer from './components/result/ResultContainer';
+import Routes from './Routes';
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router>
-      <App>
-        <Route exact path="/" component={QuizPage} />
-        <Route path="/quiz" component={QuizPage} />
-        <Route path="/result" component={ResultContainer} />
-      </App>
-    </Router>
+    <Routes />
   </Provider>,
   document.getElementById('root')
 );
