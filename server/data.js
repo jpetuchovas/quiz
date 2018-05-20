6c76e7b080e5cc4ba427521b13232eb09d39c13d
Justinas Petuchovas <justinas.petuchovas@gmail.com>
2018-05-20 08:04:18 +0300

    Add questions

--- server/data.js
+++ server/data.js
@@ -1,18 +1,31 @@
 const data = [
   {
-    question: 'question1',
-    choices: ['aaaaaaaaaaaaaaaa', 'bbbbbbbbbbbbbbbbbbbbbb', 'cccccccccccccccccc'],
+    question: 'What method is used to change state in React?',
+    choices: ['setState', 'changeState', 'replaceState'],
     answerIndex: 0,
   },
   {
-    question: 'question2',
-    choices: ['ddddddddddddddddddddddd', 'eeeeeeeeeeeeeeeeeeeeeee', 'fffffffffffffffffffff'],
-    answerIndex: 2,
+    question: 'What is an IIFE?',
+    choices: [
+      'Internally Indexed File Extension',
+      'Immediately Invoked Function Expression',
+      'Initially Integrated Functional Element',
+    ],
+    answerIndex: 1,
   },
   {
-    question: 'question3',
-    choices: ['ggggggggggggggggggggg', 'hhhhhhhhhhhhhhhhhhhh', 'iiiiiiiiiiiiiiiiiiiiiiiiii'],
+    question: 'Which of the following is an aspect of a pure function?',
+    choices: [
+      'Produces side effects',
+      "Given the same input, it'll return the same output",
+      'All of the above',
+    ],
     answerIndex: 1,
+  },
+  {
+    question: 'Which of the following values is truthy in JavaScript?',
+    choices: ['undefined', '"" (empty string)', '[] (empty array)'],
+    answerIndex: 2,
   },
 ];
 
